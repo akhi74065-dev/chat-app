f12cc0f4fa98c45b19f1f4edf14da54095e18008
akhi74065-dev <akhi74065@gmail.com>
2025-10-29 17:59:54 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
-/* Update: Added 'username' column */
+/* Update: Store sender and recipient */
 CREATE TABLE IF NOT EXISTS messages (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT NOT NULL,
+  sender_username TEXT NOT NULL,
+  recipient_username TEXT NOT NULL,
   content TEXT NOT NULL,
   timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 );
